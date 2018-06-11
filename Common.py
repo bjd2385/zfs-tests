1ef855b3aa4f9ddc1a20f3160ba59c8e5b271741
brandon <bjd2385.linux@gmail.com>
2018-06-10 21:49:17 -0400

    Fix a type error with bytes_to_mebibytes

--- Common.py
+++ Common.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 
 def bytes_to_mebibyte(size_in_bytes):
-    # type: (int) -> int
+    # type: (int) -> float
     """
     Since the output of zfs get -p is in bytes, and disk read/write is often
     measures in mb/s, this function will allow easy translation. 
